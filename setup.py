7982884cf12cd5328648809d18a42ffefbea594c
Andy Georges <itkovian@gmail.com>
2013-02-25 11:59:13 +0100

    Extracted package-specific infot from shared_setup to setup.

--- setup.py
+++ setup.py
@@ -30,29 +30,39 @@ vsc-administration distribution setup.py
 
 @author: Andy Georges (Ghent University)
 """
+import shared_setup
 from shared_setup import ag
-from shared_setup import action_target
 
-    'scripts': ['bin/logdaemon.py', 'bin/startlogdaemon.sh'],
-}
+
+def remove_bdist_rpm_source_file():
+    """List of files to remove from the (source) RPM."""
+    return ['lib/vsc/__init__.py']
+
+
+shared_setup.remove_extra_bdist_rpm_files = remove_bdist_rpm_source_file
+shared_setup.SHARED_TARGET.update({
+    'url': 'https://github.ugent.be/hpcugent/vsc-administration',
+    'download_url': 'https://github.ugent.be/hpcugent/vsc-administration'
+})
+
 
 PACKAGE = {
     'name': 'vsc-administration',
     'version': '0.3',
     'author': [ag],
     'maintainer': [ag],
-    'packages': ['vsc.administration'],
+    'packages': ['vsc', 'vsc.administration'],
     'namespace_packages': ['vsc'],
     'install_requires': [
         'vsc-base >= 0.90',
         'vsc-ldap >= 0.90',
         'vsc-ldap-extension >= 0.90',
-        'vsc-core >= 0.90',
+        'vsc-config >= 0.90',
         'vsc-packages-lockfile >= 0.9.1',
-        ],
+    ],
     'provides': ['python-vsc-administration = 0.3'],
 }
 
 
 if __name__ == '__main__':
-    action_target(PACKAGE)
+    shared_setup.action_target(PACKAGE)
