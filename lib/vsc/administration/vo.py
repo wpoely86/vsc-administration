5fb5132252166e2dad0f3e9e8e1d8e4b30b7a471
Andy Georges <itkovian@gmail.com>
2013-05-22 13:58:21 +0200

    Move the the VscStorage for deploying VO dirs. Fixes to the quota.

--- lib/vsc/administration/vo.py
+++ lib/vsc/administration/vo.py
@@ -31,7 +31,7 @@ from lockfile.pidlockfile import PIDLockFile
 from vsc import fancylogger
 from vsc.administration.institute import Institute
 from vsc.administration.user import VscUser
-from vsc.config.base import VSC, Muk, CentralStorage
+from vsc.config.base import VSC, VscStorage
 from vsc.filesystem.gpfs import GpfsOperations, PosixOperations, PosixOperationError
 from vsc.ldap.entities import VscLdapGroup
 
@@ -48,7 +48,7 @@ class VscVo(VscLdapGroup):
     A VO is a special kind of group, identified mainly by its name.
     """
 
-    def __init__(self, vo_id, storage=None):
+    def __init__(self, vo_id, storage=None, **kwargs):
         """Initialise"""
         super(VscVo, self).__init__(vo_id)
 
@@ -77,13 +77,13 @@ class VscVo(VscLdapGroup):
     def _get_path(self, storage, mount_point="gpfs"):
         """Get the path for the (if any) user directory on the given storage."""
 
-        template = self.storage.path_templates[storage]
+        template = self.storage.path_templates[storage]['vo']
         if mount_point == "login":
-            mount_path = self.storage[storage]['vo'].login_mount_point
+            mount_path = self.storage[storage].login_mount_point
         elif mount_point == "gpfs":
-            mount_path = self.storage[storage]['vo'].gpfs_mount_point
+            mount_path = self.storage[storage].gpfs_mount_point
 
-        return os.path.join(mount_path, template[0], template[1](self.user_id))
+        return os.path.join(mount_path, template[0], template[1](self.vo_id))
 
     def _data_path(self, mount_point="gpfs"):
         """Return the path to the VO data fileset on GPFS"""
@@ -97,7 +97,7 @@ class VscVo(VscLdapGroup):
         """
         return self._get_path(storage, mount_point)
 
-    def _create_fileset(self, filesystem_name, path, quota=True):
+    def _create_fileset(self, filesystem_name, path):
         """Create a fileset for the VO on the data filesystem.
 
         - creates the fileset if it does not already exist
@@ -121,19 +121,23 @@ class VscVo(VscLdapGroup):
         """Create the VO's directory on the HPC data filesystem. Always set the quota."""
         try:
             path = self._data_path()
-            self._create_fileset(self.storage.data_name, path, self.dataQuota)
-        except AttributeError, err:
-            self.log.exception("No data_name attribute in the storage instance %s" % (self.storage))
+            self._create_fileset(self.storage['VSC_DATA'].filesystem, path)
+        except AttributeError:
+            self.log.exception("Trying to access non-existant attribute in the storage instance")
+        except KeyError:
+            self.log.exception("Trying to access non-existant field in the storage dictionary")
 
-    def create_scratch_fileset(self):
+    def create_scratch_fileset(self, storage):
         """Create the VO's directory on the HPC data filesystem. Always set the quota."""
         try:
-            path = self._scratch_path()
-            self._create_fileset(self.storage.scratch_name, path, self.dataQuota)
-        except AttributeError, err:
-            self.log.exception("No scratch_name attribute in the storage instance %s" % (self.storage))
+            path = self._scratch_path(storage)
+            self._create_fileset(self.storage.[storage].filesystem, path, self.dataQuota)
+        except AttributeError:
+            self.log.exception("Trying to access non-existant attribute in the storage instance")
+        except KeyError:
+            self.log.exception("Trying to access non-existant field in the storage dictionary")
 
-    def _create_vo_dir(self, path, quota=None):
+    def _create_vo_dir(self, path):
         """Create a user owned directory on the GPFS."""
         self.gpfs.make_dir(path)
 
@@ -152,7 +156,7 @@ class VscVo(VscLdapGroup):
             # LDAP information is expressed in KiB, GPFS wants bytes.
             self.gpfs.set_fileset_quota(soft, path, self.vo_id, quota)
             self.gpfs.set_fileset_grace(path, self.vsc.vo_storage_grace_time)  # 7 days
-        except AttributeError, err:
+        except AttributeError:
             self.log.exception("No such attribute in the storage instance %s" % (self.storage))
 
     def set_data_quota(self):
@@ -162,7 +166,7 @@ class VscVo(VscLdapGroup):
         else:
             self._set_quota(self._data_path, 0)
 
-    def set_scratch_quota(self):
+    def set_scratch_quota(self, storage):
         """Set FILESET quota on the scratch FS for the VO fileset."""
         if self.scratchQuota:
             self._set_quota(self._scratch_path, int(self.scratchQuota))
@@ -204,7 +208,7 @@ class VscVo(VscLdapGroup):
         FIXME: This should probably be some variable in a config setting instance
         """
         if self.scratchQuota:
-            quota = int(self.dataQuota or 0) / 2 * 1024
+            quota = int(self.scratchQuota or 0) / 2 * 1024
         else:
             quota = 0
         self._set_member_quota(self._scratch_path, member, quota)
