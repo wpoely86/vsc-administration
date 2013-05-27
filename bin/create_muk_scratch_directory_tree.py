653daad079444989e5f05c17e669ce00728e33c3
Andy Georges <itkovian@gmail.com>
2013-05-27 17:52:56 +0200

    Added logging and addressed PR remarks

--- bin/create_muk_scratch_directory_tree.py
+++ bin/create_muk_scratch_directory_tree.py
@@ -19,7 +19,7 @@ Sets up the baseline directory structure we need to have on muk.
     - /user -> /gpfs/scratch/user
     - /apps -> /gpfs/scratch/apps
 
-@author Andy Georges
+@author: Andy Georges
 """
 
 import os
@@ -34,6 +34,12 @@ from vsc.config.base import Muk
 
 log = fancylogger.getLogger('create_directory_trees_muk')
 
+PILOT_PROJECTS = {
+    'a': 'project_apilot',
+    'b': 'project_bpilot',
+    'g': 'project_gpilot',
+    'l': 'project_lpilot',
+}
 
 def main():
     """Main."""
@@ -51,6 +57,7 @@ def main():
     if not 'user' in [f['filesetName'] for f in gpfs.gpfslocalfilesets[muk.scratch_name].values()]:
         gpfs.make_fileset(user_fileset_path, 'user')
         gpfs.chmod(0755, user_fileset_path)
+        log.info("Fileset user created and linked at %s" % (user_fileset_path))
 
     # Create the applications fileset that will be used for storing apps and tools
     # that can be used by the users on muk
@@ -58,6 +65,7 @@ def main():
     if not 'apps' in [f['filesetName'] for f in gpfs.gpfslocalfilesets[muk.scratch_name].values()]:
         gpfs.make_fileset(apps_fileset_path, 'apps')
         gpfs.chmod(0755, apps_fileset_path)
+        log.info("Fileset apps created and linked at %s" % (apps_fileset_path))
 
     # Create the projects fileset that will be used to store the directory
     # hierarchy for all project spaces on muk scratch
@@ -65,38 +73,43 @@ def main():
     if not 'projects' in [f['filesetName'] for f in gpfs.gpfslocalfilesets[muk.scratch_name].values()]:
         gpfs.make_fileset(projects_fileset_path, 'projects')
         gpfs.chmod(0755, projects_fileset_path)
+        log.info("Fileset projects created and linked at %s" % (projects_fileset_path))
 
     # If users are to log in, there should be a symlink to the GPFS directory hierarchy
     if not os.path.lexists('/user'):
         os.symlink(user_fileset_path, '/user')
+        log.info("Linking /user to %s" % (user_fileset_path))
 
     # If the apps are to be reachable in a similar vein as on the Tier-2, we need a symlink
     # from the FS root to the real fileset path
     if not os.path.lexists('/apps'):
         os.symlink(apps_fileset_path, '/apps')
+        log.info("Linking /apps to %s" % (apps_fileset_path))
 
     # In the pilot phase, we have 4 project filesets that need to be owned by the pilot groups
     # moderator and be group rw for said pilot group
-    pilot_projects = {
-        'a': 'project_apilot',
-        'b': 'project_bpilot',
-        'g': 'project_gpilot',
-        'l': 'project_lpilot',
-    }
+    pilot_projects = PILOT_PROJECTS
 
-    for institute in ['a', 'b', 'g', 'l']:
+    for institute in pilot_projects.keys():
         group_name = "%st1_mukusers" % institute
         try:
             group = l.group_filter_search(CnFilter(group_name))[0]
         except:
+            log.error("No LDAP group with the name %s found" % (group_name))
             continue
+
         owner = l.user_filter_search(CnFilter(group['moderator'][0]))[0]
 
         project_fileset_name = pilot_projects[institute]
         project_fileset_path = os.path.join(scratch['defaultMountPoint'], 'projects', project_fileset_name)
 
         if not project_fileset_name in [f['filesetName'] for f in gpfs.gpfslocalfilesets[muk.scratch_name].values()]:
-            gpfs.make_fileset(project_fileset_path, project_fileset_name)
+            try:
+                gpfs.make_fileset(project_fileset_path, project_fileset_name)
+                log.info("Created new fileset %s with link path %s" % (project_fileset_name, project_fileset_path))
+            except:
+                log.exception("Failed to create a new fileset with the name %s and link path %s" %
+                              (project_fileset_name, project_fileset_path))
 
         gpfs.chmod(0755, project_fileset_path)
         os.chown(project_fileset_path, int(owner['uidNumber']), int(group['gidNumber']))
@@ -104,6 +117,8 @@ def main():
         project_quota = 70 * 1024 * 1024 * 1024 * 1024
         gpfs.set_fileset_quota(project_quota, project_fileset_path, project_fileset_name)
 
+
+def add_
     # Exmaples
     # for a single user:
     #u = MukUser('vsc40075')  # ageorges
