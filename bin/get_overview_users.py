6c9b8a3014d7ae750cfddaafa8bbd6c4143fd679
Andy Georges <itkovian@gmail.com>
2013-05-29 10:59:44 +0200

    Get the users from the HPC Collector DB

--- bin/get_overview_users.py
+++ bin/get_overview_users.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
+##
+#
+# Copyright 2012-2013 Ghent University
+#
+# This file is part of the tools originally by the HPC team of
+# Ghent University (http://ugent.be/hpc).
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+"""
+This script produces an overview of the HPC users.
+
+@author: Andy Georges
+@author: Wouter Depypere
+"""
 
 import sys
 from collections import namedtuple
 
+from vsc.config.base import GENT
 from vsc.pg import cCol
 from vsc.ldap.configuration import VscConfiguration, UGentLdapConfiguration
 from vsc.ldap.utils import LdapQuery
-from vvsc.utils.generaloption import simple_option
+from vsc.utils.generaloption import simple_option
 
 User = namedtuple('User',[
     'vscid',
@@ -17,16 +34,13 @@ User = namedtuple('User',[
 ])
 
 
-def getAllDBusers(debug=None):
+def getAllDBusers(opts):
+    """Get the users from UGent in the HPC collector database."""
     c = cCol()
-    c.debug = debug
-    users = []
-    currentUsers = c.getlist("member", "uid, inst, active")
-    for curr_user in currentUsers:
-        if curr_user[1] == 'gent':
-            users.append(user(vscid=curr_user[0], active=curr_user[2]))
+    c.debug = opts.debug
+    users = c.getlist("member", "uid, inst, active")
 
-    return users
+    return [User(vscid=u[0], ugentid=None, active=u[2], employee=None, student=None) for u in users if u[1] == GENT]
 
 
 def getUGentID(vscuid):
@@ -82,11 +96,13 @@ def addEmplStudent(users):
     return updated_list
 
 
-if __name__ == '__main__':
+def main():
 
-    users = getAllDBusers()
-    users = addAllUGID(users)
-    users = addEmplStudent(users)
+    opts = simple_option({})  # provides debug and logging
+
+    users = getAllDBusers(opts)
+    users = addAllUGID(opts, users)
+    users = addEmplStudent(opts, users)
 
     active_users = 0
     employees = 0
@@ -161,3 +177,6 @@ if __name__ == '__main__':
     print "number of inactive people who are neither: %s" % notstudent_nor_employee_inactive
     print ""
     print "-----------------------------------------------------------------"
+
+if __name__ == '__main__':
+    main()
