c82e03c853d7959193b651e6893d74dc520d916f
Andy Georges <itkovian@gmail.com>
2013-07-19 16:44:54 +0200

    Verison bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-administration',
-    'version': '0.13',
+    'version': '0.14',
     'author': [ag],
     'maintainer': [ag],
     'packages': ['vsc', 'vsc.administration'],
