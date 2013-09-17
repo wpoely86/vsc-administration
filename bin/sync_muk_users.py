1ab0e0560493fd86a5d43774c12547c62782d8f4
Andy Georges <itkovian@gmail.com>
2013-09-17 12:25:15 +0200

    Muk users sync moved to SimpleNagios

--- bin/sync_muk_users.py
+++ bin/sync_muk_users.py
@@ -29,24 +29,18 @@ from vsc.ldap.filters import CnFilter, InstituteFilter, LdapFilter
 from vsc.ldap.utils import LdapQuery
 from vsc.ldap.timestamp import convert_timestamp, write_timestamp
 from vsc.utils import fancylogger
-from vsc.utils.availability import proceed_on_ha_service
-from vsc.utils.generaloption import simple_option
-from vsc.utils.lock import lock_or_bork, release_or_bork
-from vsc.utils.nagios import NagiosReporter, NagiosResult, NAGIOS_EXIT_OK, NAGIOS_EXIT_CRITICAL, NAGIOS_EXIT_WARNING
-from vsc.utils.timestamp_pid_lockfile import TimestampedPidLockfile
+from vsc.utils.nagios import NAGIOS_EXIT_CRITICAL
+from vsc.utils.script_tools import ExtendedSimpleOption
 
 NAGIOS_HEADER = 'sync_muk_users'
-NAGIOS_CHECK_FILENAME = "/var/cache/%s.nagios.json.gz" % (NAGIOS_HEADER)
 NAGIOS_CHECK_INTERVAL_THRESHOLD = 15 * 60  # 15 minutes
 
 SYNC_TIMESTAMP_FILENAME = "/var/run/%s.timestamp" % (NAGIOS_HEADER)
-SYNC_MUK_USERS_LOGFILE = "/var/log/%s.log" % (NAGIOS_HEADER)
 SYNC_MUK_USERS_LOCKFILE = "/gpfs/scratch/user/%s.lock" % (NAGIOS_HEADER)
 
-fancylogger.logToFile(SYNC_MUK_USERS_LOGFILE)
+logger = fancylogger.getLogger(__name__)
+fancylogger.logToScreen(True)
 fancylogger.setLogLevelInfo()
-
-logger = fancylogger.getLogger(name=NAGIOS_HEADER)
 
 
 def process_institute(options, institute, users_filter):
@@ -127,31 +121,12 @@ def main():
     """
 
     options = {
-        'dry-run': ("Do not make any updates whatsoever", None, "store_true", False),
-        'nagios': ('print out nagion information', None, 'store_true', False, 'n'),
-        'nagios-check-filename': ('filename of where the nagios check data is stored', str, 'store', NAGIOS_CHECK_FILENAME),
-        'nagios-check-interval-threshold': ('threshold of nagios checks timing out', None, 'store', NAGIOS_CHECK_INTERVAL_THRESHOLD),
-        'ha': ('high-availability master IP address', None, 'store', None),
+        'nagios-check-interval-threshold': NAGIOS_CHECK_INTERVAL_THRESHOLD,
+        'lockfile': SYNC_MUK_USERS_LOCKFILE,
     }
 
-    opts = simple_option(options)
-
-    nagios_reporter = NagiosReporter(NAGIOS_HEADER, NAGIOS_CHECK_FILENAME, NAGIOS_CHECK_INTERVAL_THRESHOLD)
-
-    if opts.options.nagios:
-        nagios_reporter.report_and_exit()
-        sys.exit(0)  # not reached
-
-    logger.info("Starting synchronisation of Muk users.")
-
-    if not proceed_on_ha_service(opts.options.ha):
-        logger.warning("Not running on the target host in the HA setup. Stopping.")
-        nagios_reporter.cache(NAGIOS_EXIT_WARNING,
-                        NagiosResult("Not running on the HA master."))
-        sys.exit(NAGIOS_EXIT_WARNING)
-
-    lockfile = TimestampedPidLockfile(SYNC_MUK_USERS_LOCKFILE)
-    lock_or_bork(lockfile, nagios_reporter)
+    opts = ExtendedSimpleOption(options)
+    stats = {}
 
     try:
         muk = Muk()
@@ -172,76 +147,22 @@ def main():
 
         muk_users_filter = LdapFilter.from_list(lambda x, y: x | y, [CnFilter(u.user_id) for u in muk_users])
 
-        users_ok = {
-            ANTWERPEN: {},
-            BRUSSEL: {},
-            GENT: {},
-            LEUVEN: {},
-        }
         for institute in nfs_mounts:
-            users_ok[institute] = process_institute(opts.options, institute, muk_users_filter)
-
-    except Exception:
-        logger.exception("Fail during muk users synchronisation")
-        nagios_reporter.cache(NAGIOS_EXIT_CRITICAL,
-                              NagiosResult("Script failed, check log file ({logfile})".format(
-                                  logfile=SYNC_MUK_USERS_LOGFILE)))
-        lockfile.release()
+            users_ok = process_institute(opts.options, institute, muk_users_filter)
+            total_institute_users = len(l.user_filter_search(InstituteFilter(institute)))
+            stats["%s_users_sync" % (institute,)] = users_ok.get(institute).get('ok',0)
+            stats["%s_users_sync_warning" % (institute,)] = int(total_institute_users / 5)  # 20% of all users want to get on
+            stats["%s_users_sync_critical" % (institute,)] = int(total_institute_users / 2)  # 30% of all users want to get on
+            stats["%s_users_sync_fail" % (institute,)] = users_ok.get(institute).get('fail',0)
+            stats["%s_users_sync_fail_warning" % (institute,)] = users_ok.get(institute).get('fail',0)
+            stats["%s_users_sync_fail_warning" % (institute,)] = 1
+            stats["%s_users_sync_fail_critical" % (institute,)] = 3
+    except Exception, err:
+        logger.exception("critical exception caught: %s" % (err))
+        opts.epilogue_critical("Script failed in a horrible way")
         sys.exit(NAGIOS_EXIT_CRITICAL)
 
-    antwerp_user_count = len(l.user_filter_search(InstituteFilter(ANTWERPEN)))
-    brussel_user_count = len(l.user_filter_search(InstituteFilter(BRUSSEL)))
-    gent_user_count = len(l.user_filter_search(InstituteFilter(GENT)))
-    leuven_user_count = len(l.user_filter_search(InstituteFilter(LEUVEN)))
-
-    # We issue a warning if more than 20% of the institute's population suddenly needs to
-    # get synched to the Tier-1; critical when 30% seems to be getting on it.
-    result_dict = {
-        'a_sync': users_ok.get(ANTWERPEN).get('ok', 0),
-        'b_sync': users_ok.get(BRUSSEL).get('ok', -1),
-        'g_sync': users_ok.get(GENT).get('ok', 0),
-        'l_sync': users_ok.get(LEUVEN).get('ok', 0),
-        'a_sync_warning': antwerp_user_count / 5,
-        'b_sync_warning': brussel_user_count / 5,
-        'g_sync_warning': gent_user_count / 5,
-        'l_sync_warning': leuven_user_count / 5,
-        'a_sync_critical': antwerp_user_count / 3,
-        'b_sync_critical': brussel_user_count / 3,
-        'g_sync_critical': gent_user_count / 3,
-        'l_sync_critical': leuven_user_count / 3,
-        'a_fail': users_ok.get(ANTWERPEN).get('fail', 0),
-        'b_fail': users_ok.get(BRUSSEL).get('fail', -1),
-        'g_fail': users_ok.get(GENT).get('fail', 0),
-        'l_fail': users_ok.get(LEUVEN).get('fail', 0),
-        'a_fail_warning': 1,
-        'b_fail_warning': 1,
-        'g_fail_warning': 1,
-        'l_fail_warning': 1,
-        'a_fail_critical': 3,
-        'b_fail_critical': 3,
-        'g_fail_critical': 3,
-        'l_fail_critical': 3,
-    }
-
-    if any([result_dict[i + '_fail'] > 0 for i in ['a', 'b', 'g', 'l']]):
-        result = NagiosResult("several users were not synched", **result_dict)
-        nagios_reporter.cache(NAGIOS_EXIT_WARNING, result)
-    else:
-        try:
-            result = NagiosResult("muk users synchronised", **result_dict)
-            (_, ldap_timestamp) = convert_timestamp()
-            if not opts.options.dry_run:
-                write_timestamp(SYNC_TIMESTAMP_FILENAME, ldap_timestamp)
-            nagios_reporter.cache(NAGIOS_EXIT_OK, result)
-        except:
-            logger.exception("Something broke writing the timestamp")
-            result.message = "muk users synchronised, filestamp not written"
-            nagios_reporter.cache(NAGIOS_EXIT_WARNING, result)
-
-    result.message = "muk users synchronised, lock release failed"
-    release_or_bork(lockfile, nagios_reporter, result)
-
-    logger.info("Finished synchronisation of muk users from LDAP with the filesystem.")
+    opts.epilogue("Muk users synchronisation completed", stats)
 
 
 if __name__ == '__main__':
