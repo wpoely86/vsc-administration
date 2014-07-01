8b2909cb286331512ba5a34dcb983f81d7aeeef7
Andy Georges <itkovian@gmail.com>
2014-07-01 11:43:20 +0200

    Version bump to the right version this time :)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-administration',
-    'version': '0.18.5',
+    'version': '0.19',
     'author': [ag],
     'maintainer': [ag],
     'packages': ['vsc', 'vsc.administration'],
