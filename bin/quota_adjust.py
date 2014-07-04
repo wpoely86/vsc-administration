a852e255960066d487babdd8324b28d102c736d8
Andy Georges <itkovian@gmail.com>
2014-07-04 16:43:22 +0200

    Reworked body to allow error handling

--- bin/quota_adjust.py
+++ bin/quota_adjust.py
@@ -14,20 +14,16 @@ This script will adjust the quota for the given entity, be it a user or a VO
 on the given storage.
 """
 
+import sys
 
-
-from vsc.accountpage.client import AccountPageClient
+from vsc.accountpage.client import AccountpageClient
 from vsc.utils.generaloption import SimpleOption
-from vsc.utils.script_tools import ExtendedSimpleOption
+
+
 
 def main():
     """
-    Main script.
-    - build the filter
-    - fetches the users
-    - process the users
-    - write the new timestamp if everything went OK
-    - write the nagios check file
+    Main script. Sets the hard limit for either user or VO.
     """
 
     options = {
@@ -35,32 +31,54 @@ def main():
         'fileset': ("The fileset where you want to adjust the quota", None, 'store', None),
         'user': ('process users', None, 'store', None),
         'vo': ('process vos', None, 'store', None),
+        'size': ('the target quota (in KiB', int, 'store', None),
+        'original': ('show the original quota values', None, 'store_true', False),
         'account_page_url': ('Base URL of the account page', None, 'store', None),  # Not used
         'access_token': ('OAuth2 token to access the account page REST API', None, 'store', None),
     }
 
     opts = SimpleOption(options)
-    client = AccountPageClient(token=opts.options.access_token)
+    client = AccountpageClient(token=opts.options.access_token)
+    client.client.url = "http://localhost:8000/api/"
 
-    fileset = opts.options.filesett
+    fileset = opts.options.fileset
     storage = opts.options.storage
+
+    if not opts.options.size:
+        sys.exit()
+
+    # TODO: could use some love in allowing a unit to be appended and converting to KiB prior to uploading
     size = int(opts.options.size)
 
     if opts.options.user:
         # quota/user/%(vsc_id)s/storage/%(storage)s/fileset/%(fileset)s/size/$
         vsc_id = opts.options.user
-        client.quota.user[vsc_id].storage[storage].fileset[fileset].size.put(hard=size)
+        original = client.account[vsc_id].quota
+        upload = client.quota.user[vsc_id].storage[storage].fileset[fileset].size
 
     if opts.options.vo:
-        # quota/vo/%(vsc_id)s/storage/%(storage)s/fileset/%(fileset)s/size/$
+
         vsc_id = opts.options.vo
-        client.quota.vo[vsc_id].storage[storage].fileset[fileset].size.put(hard=size)
+        original = client.vo[vsc_id].quota
+        upload = client.quota.vo[vsc_id].storage[storage].fileset[fileset].size
 
+    current = original.get()
+    if current[0] in (200,):
+        if opts.options.original:
+            print("Original values: %s" % (current[1],))
+    else:
+        print("Error, could not get original quota values for the given parameters")
+        print("Issue: %s" % (current[1],))
+        sys.exit(-1)
 
+    result = upload.put(body={"hard": size})
+    if result[0] in (200,):
+        print("Request OK.")
+        print("New values: %s" % (result[1],))
+    else:
+        print("Request failed")
+        print("Issue: %s" % (result[1],))
 
-
-
-
-
-
+if __name__ == '__main__':
+    main()
 
