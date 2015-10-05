cb7361e8751a25721aa4a716d1b65b40fdfe7b8c
Andy Georges <ageorges@vpna051.ugent.be>
2015-10-05 14:51:07 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ shared_setup.SHARED_TARGET.update({
 
 PACKAGE = {
     'name': 'vsc-administration',
-    'version': '0.28',
+    'version': '0.28.1',
     'author': [ag],
     'maintainer': [ag],
     'packages': ['vsc', 'vsc.administration'],
