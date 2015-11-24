ea616b689d5fe94918ff93b191c6db7893814d14
Andy Georges <itkovian@gmail.com>
2015-11-24 17:40:32 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from vsc.install import shared_setup
 from vsc.install.shared_setup import ag
 
 PACKAGE = {
-    'version': '0.28.3',
+    'version': '0.29',
     'author': [ag],
     'maintainer': [ag],
     'install_requires': [
