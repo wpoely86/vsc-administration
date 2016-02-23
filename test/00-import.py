9af311630a9af2321c5c33de1c05cf6eb91da433
Jens Timmerman <jens.timmerman@ugent.be>
2016-02-23 14:32:04 +0100

    addapted for new vsc-install

--- test/00-import.py
+++ test/00-import.py
@@ -1,8 +1,21 @@
+#
+# Copyright 2016-2016 Ghent University
+#
+# This file is part of vsc-administration,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-administration
+#
+# All rights reserved.
+#
 # Due to #11
-#from vsc.install.testing import VSCImportTest
-import vsc.install.testing
+from vsc.install import commontest
 
-class ImportTest(vsc.install.testing.VSCImportTest):
+class ImportTest(commontest.CommonTest):
     EXCLUDE_SCRIPTS = [
         r'sync_django_ldap', # requires django
         r'get_overview_users', # requires vsc.pg
--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,15 @@
+#
+# Copyright 2016-2016 Ghent University
+#
+# This file is part of vsc-administration,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-administration
+#
+# All rights reserved.
+#
 
