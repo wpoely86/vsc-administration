bcd3616c0ec741146a46bf0ce6edc3193bfbd52b
Andy Georges <ageorges@zorspro.ugent.be>
2016-07-25 13:24:20 +0200

    Updated test to use an actual VscTier2AccountpageVO instance

--- test/vo.py
+++ test/vo.py
@@ -17,13 +17,13 @@ Tests for vsc.administration.vo
 
 @author: Andy Georges (Ghent University)
 """
-import logging
 import mock
+
+from mock import patch
 
 from collections import namedtuple
 
 import vsc.administration.vo as vo
-import vsc.administration.user as user
 
 from vsc.config.base import VSC_DATA, VSC_HOME, VSC_SCRATCH_PHANPY, VSC_SCRATCH_DELCATTY
 from vsc.install.testing import TestCase
@@ -34,19 +34,18 @@ class VoDeploymentTest(TestCase):
     Tests for the VO deployment code.
     """
 
-    @mock.patch('vsc.accountpage.client.AccountpageClient', autospec=True)
-    def test_process_regular_vos(self, mock_client):
+    @patch('vsc.accountpage.client.AccountpageClient', autospec=True)
+    @patch('vsc.administration.vo.VscStorage', autospec=True)
+    def test_process_regular_vos(self, storage, mock_client):
+        """Test to see if the VscTier2AccountpageVo class is used properly"""
 
         test_vo_id = "gvo00002"
-        TestVO = namedtuple("TestVO", ['members'])
-        test_vo = TestVO(members=['vsc40001', 'vsc40002'])
         Options = namedtuple("Options", ['dry_run'])
         options = Options(dry_run=False)
 
         mc = mock_client.return_value
         mc.vo = mock.MagicMock()
         date = "20321231"
-        #mc.vo.__getitem__.return_value.members.modified.__getitem__.return_value.get.return_value = (
         mc.vo['gvo00002'].members.modified[date].get.return_value = (
             200, [{
                 u'broken': False,
@@ -68,42 +67,42 @@ class VoDeploymentTest(TestCase):
         }])
 
         for storage_name in (VSC_HOME, VSC_DATA, VSC_SCRATCH_DELCATTY, VSC_SCRATCH_PHANPY):
-            with mock.patch('vsc.administration.vo.VscTier2AccountpageVo', autospec=True) as mock_vo:
-                with mock.patch('vsc.administration.vo.VscTier2AccountpageUser', autospec=True) as mock_user:
-                    with mock.patch('vsc.administration.vo.update_vo_status') as mock_update_vo_status:
+            with mock.patch('vsc.administration.vo.VscTier2AccountpageUser', autospec=True) as mock_user:
+                with mock.patch('vsc.administration.vo.update_vo_status') as mock_update_vo_status:
+                    with mock.patch.object(vo.VscTier2AccountpageVo, 'create_scratch_fileset') as mock_cr_s_fileset:
+                        with mock.patch.object(vo.VscTier2AccountpageVo, 'set_scratch_quota') as mock_s_s_quota:
+                            with mock.patch.object(vo.VscTier2AccountpageVo, 'create_data_fileset') as mock_cr_d_fileset:
+                                with mock.patch.object(vo.VscTier2AccountpageVo, 'set_data_quota') as mock_s_d_quota:
+                                    with mock.patch.object(vo.VscTier2AccountpageVo, 'set_member_data_quota') as mock_s_m_d_quota:
+                                        with mock.patch.object(vo.VscTier2AccountpageVo, 'create_member_data_dir') as mock_cr_m_d_dir:
+                                            with mock.patch.object(vo.VscTier2AccountpageVo, 'set_member_scratch_quota') as mock_s_m_s_quota:
+                                                with mock.patch.object(vo.VscTier2AccountpageVo, 'create_member_scratch_dir') as mock_cr_m_s_dir:
+                                                    mock_user.return_value = mock.MagicMock()
+                                                    vo.process_vos(options, [test_vo_id], storage_name, mc, date)
 
-                        mock_vo.return_value = mock.MagicMock()
-                        mock_vo_instance = mock_vo.return_value
-                        mock_vo_instance.vo = test_vo
-                        mock_vo_instance.vsc_id = test_vo_id
-                        mock_user.return_value = mock.MagicMock()
+                                                    if storage_name in (VSC_HOME, VSC_DATA):
+                                                        mock_cr_s_fileset.assert_not_called()
+                                                        mock_s_s_quota.assert_not_called()
 
-                        vo.process_vos(options, [test_vo_id], storage_name, mc, date)
+                                                    if storage_name in (VSC_DATA,):
+                                                        self.assertEqual(mock_cr_d_fileset.called, True)
+                                                        self.assertEqual(mock_s_d_quota.called, True)
+                                                        self.assertEqual(mock_update_vo_status.called, True)
 
-                        if storage_name in (VSC_HOME, VSC_DATA):
-                            mock_vo_instance.create_scratch_fileset.assert_not_called()
-                            mock_vo_instance.set_scratch_quota.assert_not_called()
+                                                        self.assertEqual(mock_s_m_d_quota.called, True)
+                                                        self.assertEqual(mock_cr_m_d_dir.called, True)
 
-                        if storage_name in (VSC_DATA,):
-                            self.assertEqual(mock_vo_instance.create_data_fileset.called, True)
-                            self.assertEqual(mock_vo_instance.set_data_quota.called, True)
-                            self.assertEqual(mock_update_vo_status.called, True)
+                                                    else:
+                                                        mock_cr_d_fileset.assert_not_called()
+                                                        mock_s_d_quota.assert_not_called()
+                                                        mock_update_vo_status.assert_not_called()
 
-                            self.assertEqual(mock_vo_instance.set_member_data_quota.called, True)
-                            self.assertEqual(mock_vo_instance.create_member_data_dir.called, True)
+                                                        if storage_name not in (VSC_HOME,):
+                                                            self.assertEqual(mock_cr_s_fileset.called, True)
+                                                            self.assertEqual(mock_s_s_quota.called, True)
 
-                        else:
-                            mock_vo_instance.create_data_fileset.assert_not_called()
-                            mock_vo_instance.set_data_quota.assert_not_called()
-                            mock_update_vo_status.assert_not_called()
-
-                            if storage_name not in (VSC_HOME,):
-                                self.assertEqual(mock_vo_instance.create_scratch_fileset.called, True)
-                                self.assertEqual(mock_vo_instance.set_scratch_quota.called, True)
-
-                                self.assertEqual(mock_vo_instance.set_member_scratch_quota.called, True)
-                                self.assertEqual(mock_vo_instance.create_member_scratch_dir.called, True)
-
+                                                            self.assertEqual(mock_s_m_s_quota.called, True)
+                                                            self.assertEqual(mock_cr_m_s_dir.called, True)
 
     @mock.patch('vsc.accountpage.client.AccountpageClient', autospec=True)
     def test_process_non_gent_institute_vos(self, mock_client):
@@ -145,7 +144,7 @@ class VoDeploymentTest(TestCase):
                         mock_vo.return_value = mock.MagicMock()
                         mock_vo_instance = mock_vo.return_value
                         mock_vo_instance.vo = test_vo
-                        mock_vo_instance.vsc_id = test_vo_id
+                        mock_vo_instance.vo_id = test_vo_id
                         mock_user.return_value = mock.MagicMock()
 
                         vo.process_vos(options, [test_vo_id], storage_name, mc, "99991231")
