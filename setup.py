7549d7e58fd87d3a2e9cda98ba55599bb841423a
Andy Georges <itkovian@gmail.com>
2017-01-06 10:26:51 +0100

    Add minor version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from vsc.install import shared_setup
 from vsc.install.shared_setup import ag
 
 PACKAGE = {
-    'version': '0.36',
+    'version': '0.36.0',
     'author': [ag],
     'maintainer': [ag],
     'tests_require': ['mock'],
