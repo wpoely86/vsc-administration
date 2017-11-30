5404156efd892c38b69a874e3a5bd563f3b64df6
Andy Georges <itkovian@gmail.com>
2017-11-30 15:16:58 +0100

    Version bump to 1.2.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from vsc.install import shared_setup
 from vsc.install.shared_setup import ag, jt
 
 PACKAGE = {
-    'version': '1.1.2',
+    'version': '1.2.0',
     'author': [ag, jt],
     'maintainer': [ag, jt],
     'tests_require': ['mock'],
