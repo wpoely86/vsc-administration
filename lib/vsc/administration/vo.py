e877d7409b6eb9f06a580a4fa0a2b0eb2449c719
Andy Georges <itkovian@gmail.com>
2018-01-26 14:45:15 +0100

    Get data quota for the VO the member belongs to

--- lib/vsc/administration/vo.py
+++ lib/vsc/administration/vo.py
@@ -367,9 +367,10 @@ class VscTier2AccountpageVo(VscAccountPageVo):
             return
 
         if member.vo_data_quota:
-            logging.info("Setting the data quota for VO %s member %s to %d GiB" %
-                         (self.vo.vsc_id, member.account.vsc_id, member.vo_data_quota[0].hard))
-            self._set_member_quota(VSC_DATA, self._data_path(), member, member.vo_data_quota[0].hard)
+            quota = [q for q in member.vo_data_quota if q.fileset == self.vo.vsc_id]
+            logging.info("Setting the data quota for VO %s member %s to %d KiB" %
+                         (self.vo.vsc_id, member.account.vsc_id, quota[0].hard))
+            self._set_member_quota(VSC_DATA, self._data_path(), member, quota[0].hard)
         else:
             logging.error("No VO %s data quota set for member %s" % (self.vo.vsc_id, member.account.vsc_id))
 
