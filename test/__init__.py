362dab8669141eec7d732d96d8df174b872276fd
Andy Georges <itkovian@gmail.com>
2018-01-31 10:10:52 +0100

    Fixed header. Again

--- test/__init__.py
+++ test/__init__.py
@@ -12,18 +12,4 @@
 #
 # All rights reserved.
 #
-#
-# Copyright 2016-2017 Ghent University
-#
-# This file is part of vsc-administration,
-# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
-# with support of Ghent University (http://ugent.be/hpc),
-# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
-# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
-# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
-#
-# https://github.com/hpcugent/vsc-administration
-#
-# All rights reserved.
-#
 
