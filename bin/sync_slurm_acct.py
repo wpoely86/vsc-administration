fb46d24dd9a0c4ae21d9c6cc845657b7671f478e
Andy Georges <itkovian@gmail.com>
2018-05-16 15:50:30 +0200

    Parse sacctmgr output

--- bin/sync_slurm_acct.py
+++ bin/sync_slurm_acct.py
@@ -19,15 +19,18 @@ This script synchronises the users and VO's from the HPC account page to the Slu
 The script must result in an idempotent execution, to ensure nothing breaks.
 """
 
+import logging
 import sys
 import tempfile
+import subprocess
 
-from collections import namedtuple
+from collections import namedtuple, Mapping
 from datetime import datetime
 
 from vsc.accountpage.client import AccountpageClient
 from vsc.accountpage.wrappers import mkVo
-from vsc.ldap.timestamp import convert_timestamp, read_timestamp, write_timestamp
+from vsc.accountpage.wrappers import mkNamedTupleInstance
+from vsc.utils.timestamp import convert_timestamp, read_timestamp, write_timestamp
 from vsc.utils import fancylogger
 from vsc.utils.missing import nub
 from vsc.utils.nagios import NAGIOS_EXIT_CRITICAL
@@ -46,12 +49,32 @@ logger = fancylogger.getLogger()
 fancylogger.logToScreen(True)
 fancylogger.setLogLevelInfo()
 
+ACCOUNTS = "accounts"
+USERS = "users"
 
-from vsc.accountpage.wrappers import mkNamedTupleInstance
+
+# https://stackoverflow.com/questions/11351032/namedtuple-and-default-values-for-optional-keyword-arguments
+def namedtuple_with_defaults(typename, field_names, default_values=()):
+    T = namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
 
 
-SlurmAccount = namedtuple('SlurmAccount', ['Description', 'Organization', 'Fairshare'])
-SlurmUser = namedtuple('SlurmUser', ['DefaultAccount', 'Fairshare'])
+SacctUserFields = ["User", "Def_Acct", "Admin", "Cluster", "Account", "Partition", "Share",
+                   "MaxJobs", "MaxNodes", "MaxCPUs", "MaxSubmit", "MaxWall", "MaxCPUMins",
+                   "QOS", "Def_QOS"]
+SacctAccountFields = ["Account", "Descr", "Org", "Cluster", "Par_Name", "User", "Share",
+                      "GrpJobs", "GrpNodes", "GrpCPUs", "GrpMem", "GrpSubmit", "GrpWall", "GrpCPUMins",
+                      "MaxJobs", "MaxNodes", "MaxCPUs", "MaxSubmit", "MaxWall", "MaxCPUMins",
+                      "QOS", "Def_QOS"]
+
+SlurmAccount = namedtuple_with_defaults('SlurmAccount', SacctAccountFields)
+SlurmUser = namedtuple_with_defaults('SlurmUser', SacctUserFields)
 
 
 def mkSlurmAccount(fields):
@@ -62,43 +85,69 @@ def mkSlurmUser(fields):
     mkNamedTupleInstance(fields, SlurmUser)
 
 
-def parse_slurm_acct_line(line):
-    return ('Account', 'gvo00002', {'Description': 'gvo00002', 'Organization': 'gvo00002', 'Fairshare': 1})
+def parse_slurm_acct_line(header, line, creator):
+    fields = line.split("|")
+    print "fields (%d): %s" % (len(fields), fields)
+    return creator(dict(zip(header, fields)))
 
 
-def parse_slurm_acct_dump(lines):
+def parse_slurm_acct_dump(lines, info_type):
     """
-    Parse the accounts and users from the dump
-
-    sacctmgr: Account - 'gvo00002':Description='gvo00002':Organization='gvo00002':Fairshare=1
-    sacctmgr: User - 'vsc40002':DefaultAccount='gvo00002':Fairshare=1
+    Parse the accounts from the listing
     """
-    accounts = set()
-    users = set()
+    info = set()
 
-    print "Processing %d lines" % len(lines)
-    for l in lines:
-        (kind, name, fields) = parse_slurm_acct_line(l)
-        if kind == 'Account':
-            accounts.add((name, mkSlurmAccount(fields)))
-        if kind == 'User':
-            users.add((name, mkSlurmUser(fields)))
+    if info_type == ACCOUNTS:
+        creator = mkSlurmAccount
+    elif info_type == USERS:
+        creator = mkSlurmUser
 
-    return (accounts, users)
+    header = [w.replace(' ', '_') for w in lines[0].rstrip().split("|")]
 
-def get_slurm_account_info(cluster):
-    """Get slurm account info for the given cluster"""
+    print "Headers: %s" % header
 
+    for line in lines[1:]:
+        line = line.rstrip()
+        try:
+            info.add(parse_slurm_acct_line(header, line, creator))
+        except Exception, err:
+            logging.warning("Slurm acct sync: could not process line %s [%s]", line, err)
+
+    return info
+
+
+def get_slurm_acct_info(info_type):
+    """Get slurm account info for the given clusterself.
+
+    @param cluster: the cluster for which to get information
+    @param info_type: this is either "accounts" or "users"
+    """
     contents = None
-    with tempfile.NamedTemporaryFile() as f:
-        (ec, output) = run_simple([SLURM_SACCT_MGR, "dump", cluster, "File=%s" % f.name])
+    outputFile = tempfile.NamedTemporaryFile(delete=True)
+    with open(outputFile.name, 'r+') as f:
+        try:
+            subprocess.check_call([
+                SLURM_SACCT_MGR,
+                "-s",
+                "-P",
+                "list",
+                "%s" % info_type,
+                ],
+                stdout=f
+            )
+        except subprocess.CalledProcessError, err:
+            logging.error("Could not get sacctmgr output: error %d", err.returncode)
+            raise
+
         f.flush()
         f.seek(0)
         contents = f.readlines()
-        print "ec: %s" % ec
         print "read %d lines" % len(contents)
 
-    (accounts, users) = parse_slurm_acct_dump(contents)
+    print "header: %s" % contents[0]
+    info = parse_slurm_acct_dump(contents, info_type)
+
+    return info
 
 
 def main():
@@ -110,7 +159,7 @@ def main():
         'nagios-check-interval-threshold': NAGIOS_CHECK_INTERVAL_THRESHOLD,
         'access_token': ('OAuth2 token to access the account page REST API', None, 'store', None),
         'account_page_url': ('URL of the account page where we can find the REST API', str, 'store', 'https://apivsc.ugent.be/django'),
-        'cluster': ('Cluster to get the info for', str, 'store', 'banette'),
+        'clusters': ('Cluster(s) (comma-separated) to get the info for', str, 'store', 'banette'),
     }
 
     opts = ExtendedSimpleOption(options)
@@ -118,15 +167,26 @@ def main():
 
     try:
         now = datetime.utcnow()
-        client = AccountpageClient(token=opts.options.access_token, url=opts.options.account_page_url + "/api/")
+        client = AccountpageClient(
+            token=opts.options.access_token,
+            url=opts.options.account_page_url + "/api/")
 
         try:
             last_timestamp = read_timestamp(SYNC_TIMESTAMP_FILENAME)
         except Exception:
-            logger.exception("Something broke reading the timestamp from %s" % SYNC_TIMESTAMP_FILENAME)
+            logging.exception("Error reading from %s" % SYNC_TIMESTAMP_FILENAME)
             last_timestamp = "200901010000Z"  # the beginning of time
+        if last_timestamp is None:
+            last_timestamp = "201804010000Z"  # the beginning of time
+        last_timestamp = "201804010000Z"  # the beginning of time
 
-        logger.info("Last recorded timestamp was %s" % (last_timestamp))
+        logging.info("Last recorded timestamp was %s" % (last_timestamp))
+
+        slurm_account_info = get_slurm_acct_info(ACCOUNTS)
+        slurm_users_info = get_slurm_acct_info(USERS)
+
+        print slurm_account_info
+        print slurm_users_info
 
         # All users belong to a VO, so fetching the VOs is necessary and sufficient.
         # This assumes correct timestamps on the VOs if members join and leave
@@ -136,7 +196,7 @@ def main():
         for vo in account_page_vos:
             account_page_members[vo.vsc_id] = vo.members
 
-        slurm_account_info = get_slurm_account_info(opts.options.cluster)
+        
 
     except Exception as err:
         logger.exception("critical exception caught: %s" % (err))
