d64382030d7edf98ef415e994250c25c847158c8
Andy Georges <itkovian@gmail.com>
2018-05-24 16:02:49 +0200

    Init

---
+++ lib/vsc/administration/slurm/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: latin-1 -*-
+#
+# Copyright 2018-2018 Ghent University
+#
+# This file is part of vsc-administration,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-administration
+#
+# All rights reserved.
+#
+"""
+This module provides support for all things slurm and VSC accounts.
+
+@author Andy Georges
+
+@created May 24, 2018
+"""
+#the vsc namespace is used in different folders allong the system
+#so explicitly declare this is also the vsc namespace
+import pkg_resources
+pkg_resources.declare_namespace(__name__)
+
+__author__ = 'ageorges'
+__date__ = 'May 24, 2018'
