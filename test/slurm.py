54345a5d56796670a898a380f1a05f1f40ed615e
Andy Georges <itkovian@gmail.com>
2018-05-24 16:48:07 +0200

    Test for slurm acct parsing

--- test/slurm.py
+++ test/slurm.py
@@ -22,7 +22,8 @@ from collections import namedtuple
 
 from vsc.install.testing import TestCase
 
-from vsc.administration.slurm.sync import slurm_vo_accounts
+from vsc.administration.slurm.sync import slurm_vo_accounts, slurm_user_accounts, parse_slurm_acct_dump
+from vsc.administration.slurm.sync import ACCOUNTS, USERS, SlurmAccount, SlurmUser
 
 
 VO = namedtuple("VO", ["vsc_id", "institute"])
@@ -32,6 +33,7 @@ class SlurmSyncTest(TestCase):
     """Test for the slurm account sync."""
 
     def test_slurm_vo_accounts(self):
+        """Test that the commands to create accounts are correctly generated."""
 
         vos = [
             VO(vsc_id="gvo00001", institute={"site": "gent"}),
@@ -48,3 +50,70 @@ class SlurmSyncTest(TestCase):
             "/usr/bin/sacctmgr add account gvo00001 Parent=gent Organization=ugent Cluster=mycluster",
             "/usr/bin/sacctmgr add account gvo00002 Parent=gent Organization=ugent Cluster=mycluster",
         ])
+
+    def test_slurm_user_accounts(self):
+        """Test that the commands to create, change and remove users are correctly generated."""
+        vo_members = {
+            "vo1": (["user1", "user2", "user3"], VO(vsc_id="vo1", institute={"site": "gent"})),
+            "vo2": (["user4", "user5", "user6"], VO(vsc_id="vo2", institute={"site": "gent"})),
+        }
+
+        active_accounts = ["user1", "user3", "user4", "user5", "user6", "user7"]
+        slurm_user_info = [
+            SlurmUser(User='user1', Def_Acct='vo1', Admin='None', Cluster='banette', Account='vo1', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='user2', Def_Acct='vo1', Admin='None', Cluster='banette', Account='vo1', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='user3', Def_Acct='vo2', Admin='None', Cluster='banette', Account='vo2', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='user4', Def_Acct='vo1', Admin='None', Cluster='banette', Account='vo1', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='user5', Def_Acct='vo2', Admin='None', Cluster='banette', Account='vo2', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+        ]
+
+        commands = slurm_user_accounts(vo_members, active_accounts, slurm_user_info, ["banette"])
+
+        self.assertEqual(set(commands), set([
+            "/usr/bin/sacctmgr add user user6 Account=vo2 Cluster=banette",
+            "/usr/bin/sacctmgr delete user name=user2 Cluster=banette",
+            "/usr/bin/sacctmgr update user=user3 where Cluster=banette set DefaultAccount=vo1 Account=vo1",
+            "/usr/bin/sacctmgr update user=user4 where Cluster=banette set DefaultAccount=vo2 Account=vo2",
+        ]))
+
+
+    def test_parse_slurmm_acct_dump(self):
+        """Test that the sacctmgr output is correctly processed."""
+
+        sacctmgr_account_output = [
+            "Account|Descr|Org|Cluster|Par Name|User|Share|GrpJobs|GrpNodes|GrpCPUs|GrpMem|GrpSubmit|GrpWall|GrpCPUMins|MaxJobs|MaxNodes|MaxCPUs|MaxSubmit|MaxWall|MaxCPUMins|QOS|Def QOS",
+            "antwerpen|antwerpen|uantwerpen|banette|root||1||||||||||||||normal|",
+            "brussel|brussel|vub|banette|root||1||||||||||||||normal|",
+            "gent|gent|ugent|banette|root||1||||||||||||||normal|",
+            "vo1|vo1|ugent|banette|gent||1||||||||||||||normal|",
+            "vo2|vo2|ugent|banette|gent||1||||||||||||||normal|",
+            "vo2|vo2|gvo00002|banette||someuser|1||||||||||||||normal|",
+        ]
+
+        info = parse_slurm_acct_dump(sacctmgr_account_output, ACCOUNTS)
+
+        self.assertEqual(set(info), set([
+            SlurmAccount(Account='brussel', Descr='brussel', Org='vub', Cluster='banette', Par_Name='root', User='', Share='1', GrpJobs='', GrpNodes='', GrpCPUs='', GrpMem='', GrpSubmit='', GrpWall='', GrpCPUMins='', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmAccount(Account='gent', Descr='gent', Org='ugent', Cluster='banette', Par_Name='root', User='', Share='1', GrpJobs='', GrpNodes='', GrpCPUs='', GrpMem='', GrpSubmit='', GrpWall='', GrpCPUMins='', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmAccount(Account='vo2', Descr='vo2', Org='ugent', Cluster='banette', Par_Name='gent', User='', Share='1', GrpJobs='', GrpNodes='', GrpCPUs='', GrpMem='', GrpSubmit='', GrpWall='', GrpCPUMins='', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmAccount(Account='antwerpen', Descr='antwerpen', Org='uantwerpen', Cluster='banette', Par_Name='root', User='', Share='1', GrpJobs='', GrpNodes='', GrpCPUs='', GrpMem='', GrpSubmit='', GrpWall='', GrpCPUMins='', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmAccount(Account='vo1', Descr='vo1', Org='ugent', Cluster='banette', Par_Name='gent', User='', Share='1', GrpJobs='', GrpNodes='', GrpCPUs='', GrpMem='', GrpSubmit='', GrpWall='', GrpCPUMins='', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS='')
+        ]))
+
+        sacctmgr_user_output = [
+            "User|Def Acct|Admin|Cluster|Account|Partition|Share|MaxJobs|MaxNodes|MaxCPUs|MaxSubmit|MaxWall|MaxCPUMins|QOS|Def QOS",
+            "root|root|Administrator|banette|root||1|||||||normal|",
+            "root|root|Administrator|banette2|root||1|||||||normal|",
+            "root|root|Administrator|banette3|root||1|||||||normal|",
+            "account1|vo1|None|banette|vo1||1|||||||normal|",
+            "account2|vo1|None|banette|vo1||1|||||||normal|",
+            "account3|vo2|None|banette|vo2||1|||||||normal|",
+        ]
+
+        info = parse_slurm_acct_dump(sacctmgr_user_output, USERS)
+
+        self.assertEqual(set(info), set([
+            SlurmUser(User='account1', Def_Acct='vo1', Admin='None', Cluster='banette', Account='vo1', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='account2', Def_Acct='vo1', Admin='None', Cluster='banette', Account='vo1', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+            SlurmUser(User='account3', Def_Acct='vo2', Admin='None', Cluster='banette', Account='vo2', Partition='', Share='1', MaxJobs='', MaxNodes='', MaxCPUs='', MaxSubmit='', MaxWall='', MaxCPUMins='', QOS='normal', Def_QOS=''),
+        ]))
