bb0efabb99f5e95622d03a36abb50884cac9c774
Andy Georges <itkovian@gmail.com>
2018-05-24 15:35:34 +0200

    Tests for slurm sync - empty test atm

---
+++ test/slurm.py
@@ -0,0 +1,27 @@
+#
+# Copyright 2015-2018 Ghent University
+#
+# This file is part of vsc-administration,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/hpcugent/vsc-administration
+#
+# All rights reserved.
+#
+"""
+Tests for vsc.administration.slurm.*
+
+@author: Andy Georges (Ghent University)
+"""
+
+
+from vsc.install.testing import TestCase
+
+from vsc.administration.slurm.sync import *
+
+class SlurmSyncTest(TestCase):
+    pass
