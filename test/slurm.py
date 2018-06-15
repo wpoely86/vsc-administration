53566083a936d6aeb5d4a46aaf56167aa04fe9b4
Andy Georges <itkovian@gmail.com>
2018-06-15 16:21:53 +0200

    Update is replaced by add + delete

--- test/slurm.py
+++ test/slurm.py
@@ -71,10 +71,12 @@ class SlurmSyncTest(TestCase):
         commands = slurm_user_accounts(vo_members, active_accounts, slurm_user_info, ["banette"])
 
         self.assertEqual([tuple(x) for x in commands], [tuple(x) for x in [
-            shlex.split("/usr/bin/sacctmgr add user user6 Account=vo2 Cluster=banette"),
+            shlex.split("/usr/bin/sacctmgr add user user6 Account=vo2 DefaultAccount=vo2 Cluster=banette"),
             shlex.split("/usr/bin/sacctmgr delete user name=user2 Cluster=banette"),
-            shlex.split("/usr/bin/sacctmgr update user=user3 where Cluster=banette set DefaultAccount=vo1 Account=vo1"),
-            shlex.split("/usr/bin/sacctmgr update user=user4 where Cluster=banette set DefaultAccount=vo2 Account=vo2"),
+            shlex.split("/usr/bin/sacctmgr add user user3 Account=vo1 DefaultAccount=vo1 Cluster=banette"),
+            shlex.split("/usr/bin/sacctmgr delete user name=user3 Account=vo2 Cluster=banette")
+            shlex.split("/usr/bin/sacctmgr add user user4 Account=vo2 DefaultAccount=vo2 Cluster=banette"),
+            shlex.split("/usr/bin/sacctmgr delete user name=user4 Account=vo1 Cluster=banette")
         ]])
 
 
