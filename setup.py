74658e64add626b5e775a90a6d9cf51503c8571a
Andy Georges <itkovian@gmail.com>
2019-01-14 17:00:50 +0100

    Version bump to 1.8.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from vsc.install import shared_setup
 from vsc.install.shared_setup import ag, jt
 
 PACKAGE = {
-    'version': '1.8.1',
+    'version': '1.8.2',
     'author': [ag, jt],
     'maintainer': [ag, jt],
     'tests_require': ['mock'],
