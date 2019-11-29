71b3e3372297f985866e2451b80cd4d0de589300
Ward Poelmans <wpoely86@gmail.com>
2019-11-29 08:18:01 +0100

    Use create_state_directories from vsc.filesystem

--- lib/vsc/administration/tools.py
+++ lib/vsc/administration/tools.py
@@ -19,10 +19,7 @@ Original Perl code by Stijn De Weirdt
 
 @author: Andy Georges (Ghent University)
 """
-
 import logging
-from os import stat as os_stat
-import stat
 
 TIER1_GRACE_GROUP_SUFFIX = "t1_mukgraceusers"
 TIER1_HELPDESK_ADDRESS = "tier1@ugent.be"
@@ -41,29 +38,10 @@ Kind regards,
 -- The UGent HPC team
 """
 
+
 def create_stat_directory(path, permissions, uid, gid, posix, override_permissions=True):
     """
-    Create a new directory if it does not exist and set permissions, ownership. Otherwise,
-    check the permissions and ownership and change if needed.
+    Function moved to vsc-filesystems
     """
-    created = False
-    try:
-        statinfo = os_stat(path)
-        logging.debug("Path %s found.", path)
-    except OSError:
-        created = posix.make_dir(path)
-        logging.info("Created directory at %s" % (path,))
-
-    if created or (override_permissions and stat.S_IMODE(statinfo.st_mode) != permissions):
-        posix.chmod(permissions, path)
-        logging.info("Permissions changed for path %s to %s", path, permissions)
-    else:
-        logging.debug("Path %s already exists with correct permissions" % (path,))
-
-    if created or statinfo.st_uid != uid or statinfo.st_gid != gid:
-        posix.chown(uid, gid, path)
-        logging.info("Ownership changed for path %s to %d, %d", path, uid, gid)
-    else:
-        logging.debug("Path %s already exists with correct ownership" % (path,))
-
-    return created
+    logging.warning("The create_stat_directory function has moved to vsc.filesystems")
+    return posix.create_stat_directory(path, permissions, uid, gid, override_permissions=override_permissions)
