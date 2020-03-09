0faa9de1fdad7272fa82c49e92a781cbb22e3a8b
Ward Poelmans <wpoely86@gmail.com>
2020-03-09 13:37:38 +0100

    Add username when logging quota changes

--- lib/vsc/administration/user.py
+++ lib/vsc/administration/user.py
@@ -354,7 +354,7 @@ class VscTier2AccountpageUser(VscAccountPageUser):
         quota = hard * 1024 * self.institute_storage[storage_name].data_replication_factor
         soft = int(self.vsc.quota_soft_fraction * quota)
 
-        logging.info("Setting quota for %s on %s to %d", storage_name, path, quota)
+        logging.info("Setting quota for %s - %s on %s to %d", self.account.vsc_id, storage_name, path, quota)
 
         # LDAP information is expressed in KiB, GPFS wants bytes.
         self.gpfs.set_user_quota(soft, int(self.account.vsc_id_number), path, quota)
